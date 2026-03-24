e21055d0d76680ef8874b13dc1a07f13e5ff5e47
JECHEATCOMMEUNPORC <ayoubchehli1977@gmail.com>
2026-03-24 15:22:13 +0100

    Add files via upload

---
+++ mon/projet/MonProjet.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mon.projet;
+
+/**
+ *
+ * @author Houda
+ */
+public class MonProjet {
+
+ 
+    public static void main(String[] args) {
+        int score = 0;
+        score = score + 1;
+        System.out.println("Ton score est de : " + score);
+    }
+}
+    
+    
+
